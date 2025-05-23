1ca9d92e5fa5520829e0f8163c3513c9b9c17a8b
shanidan92 <shanidan92@gmail.com>
2025-05-23 16:17:09 +0300

    lint

--- app/appData/musicStore.ts
+++ app/appData/musicStore.ts
@@ -1,33 +1,29 @@
-import { create } from 'zustand';
-import type { Album, Artist, MusicListSection, SongDetails } from './models';
+import { create } from "zustand";
+import type { Album, Artist, MusicListSection, SongDetails } from "./models";
 
 type musicLibraryStore = {
-    newReleases: Album[];
-    recentlyPlayed: SongDetails[],
-    artists: Artist[],
-    labels: Artist[],
-    musicSections: MusicListSection[],
+  newReleases: Album[];
+  recentlyPlayed: SongDetails[];
+  artists: Artist[];
+  labels: Artist[];
+  musicSections: MusicListSection[];
 };
 
 export const useMusicLibraryStore = create<musicLibraryStore>((set) => ({
-    newReleases: [], 
-    recentlyPlayed: [],
-    artists: [],
-    labels: [],
-    musicSections: [],
+  newReleases: [],
+  recentlyPlayed: [],
+  artists: [],
+  labels: [],
+  musicSections: [],
 
-    setNewReleases: (albums: Album[]) =>
-        set({ newReleases: albums }),
+  setNewReleases: (albums: Album[]) => set({ newReleases: albums }),
 
-    setRecentlyPlayed: (songs: SongDetails[]) =>
-        set({ recentlyPlayed: songs }),
+  setRecentlyPlayed: (songs: SongDetails[]) => set({ recentlyPlayed: songs }),
 
-    setArtists: (artists: Artist[]) =>
-        set({ artists }),
+  setArtists: (artists: Artist[]) => set({ artists }),
 
-    setLabels: (labels: Artist[]) =>
-        set({ labels }),
+  setLabels: (labels: Artist[]) => set({ labels }),
 
-    setMusicSections: (sections: MusicListSection[]) =>
-        set({ musicSections: sections }),
+  setMusicSections: (sections: MusicListSection[]) =>
+    set({ musicSections: sections }),
 }));
