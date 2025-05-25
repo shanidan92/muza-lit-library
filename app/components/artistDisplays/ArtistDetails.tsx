335e3e14d75f370fc5378f476f1d9c517164229e
Jon Shamir <jon.shamir@gmail.com>
2025-05-25 17:35:32 +0300

    Artist items

--- app/components/artistDisplays/ArtistDetails.tsx
+++ app/components/artistDisplays/ArtistDetails.tsx
@@ -21,17 +21,6 @@ const ArtistDetails: React.FC<ArtistDetailsProps> = ({ details }) => {
       <div className="info">
         <div className="title">{details.artistName}</div>
         <div className="subtitle">{details.albumsCount} Albums</div>
-        <div className="buttons">
-          <button className="icon-button">
-            <span className="icon icon-dots" />
-          </button>
-          <button className="icon-button">
-            <span className="icon icon-info" />
-          </button>
-          <button className="icon-button">
-            <span className="icon icon-plus" />
-          </button>
-        </div>
       </div>
     </div>
   );
