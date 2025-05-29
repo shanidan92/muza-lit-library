cadb5f0345c51f4feac8267eb5344dde2d3df868
shanidan92 <shanidan92@gmail.com>
2025-05-29 18:28:40 +0300

    get db data

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,11 @@
 import express from "express";
 // import cors from 'cors'
 import { fileURLToPath } from 'url';
+import axios from 'axios';
 import path from 'path';
+import https from 'https';
+import * as fs from 'fs';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,18 +16,41 @@ const publicDir = path.resolve('./public');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+ 
+const instance = axios.create({ httpsAgent })
 
-// app.use(cors({
-//   origin: [ 'http://localhost:5173'],
-//   credentials: true,
-// }));
+const filePath =path.join(publicDir, '/mockData/allData.json'); // Path to your JSON file
+let allData = {};
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
+  }
+  try {
+    allData = JSON.parse(data);
+  } catch (parseError) {
+    console.error('Error parsing JSON:', parseError);
+  }
+});
+ const data = await instance.post('https://ec2-34-244-32-40.eu-west-1.compute.amazonaws.com/api/metadata/graphql',
+    { "query": "{ allTracks { id uuid songTitle artistMain bandName albumTitle yearRecorded yearReleased instrument otherArtistPlaying otherInstrument composer label createdAt}}" }, { httpsAgent })
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+    
 app.get('/getAllData', (req, res) => {
-  res.sendFile(path.join(publicDir, '/mockData/allData.json'));
+  const response  = {...allData , tracks: data.data.allTracks}
+  res.send(response);
+
 })
 app.use(express.static(clientDir));
 
-app.get("/" , (req,res)=>{
-   res.sendFile(path.join(clientDir, 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(clientDir, 'index.html'));
 })
 
 
